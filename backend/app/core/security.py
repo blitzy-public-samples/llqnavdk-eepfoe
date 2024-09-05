9c3fcf7125f6b30a071a4dbd98eed4062e13d4a6
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-05 10:12:48 -0400

    Add security.py

---
+++ backend/app/core/security.py
@@ -0,0 +1,26 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+import jwt
+from backend.app.core.config import Settings
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+settings = Settings()
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
+
+def decode_access_token(token: str) -> dict:
+    return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
